7b9b9fc35e13b333d0678dddca9a0e8ba931c88a
Ahmed Idris <114231146+AhmedIdris90@users.noreply.github.com>
2022-09-23 17:34:53 +0200

    Add files via upload

--- Covid Data.sql
+++ Covid Data.sql
@@ -12,18 +12,21 @@ ORDER BY 1,2;
 
 -- Looking at Total Cases VS Total Deaths
 -- Shows Likelihood of dying if you contract Covid in your country
+
 SELECT Location,date,total_cases, total_deaths, (total_deaths/total_cases)*100 AS DeathPercentage
 FROM CovidDeaths.coviddeaths
 WHERE location LIKE '%states%'
 ORDER BY 1,2;
 
 -- Looking at Total Cases VS Population
+
 SELECT Location,date,total_cases, population, (total_cases/population)*100 AS CasesPercentage
 FROM CovidDeaths.coviddeaths
 WHERE location LIKE '%states%'
 ORDER BY 1,2;
 
 -- Looking at Countries with highest infection rates
+
 SELECT Location, MAX(total_cases)as HighestInfectionCount, population, MAX((total_cases/population))*100 AS CasesPercentage
 FROM CovidDeaths.coviddeaths
 -- WHERE location LIKE '%states%'
@@ -31,6 +34,7 @@ GROUP BY location, population
 ORDER BY CasesPercentage DESC;
 
 -- Showing Countries with highest death rates
+
 SELECT Location, MAX(total_deaths) as HighestDeathCount
 FROM CovidDeaths.coviddeaths
 WHERE Continent != ""
@@ -38,6 +42,7 @@ GROUP BY location
 ORDER BY HighestDeathCount DESC;
 
 -- Showing Continents with highest death rates
+
 SELECT continent, MAX(total_deaths) as HighestDeathCount
 FROM CovidDeaths.coviddeaths
 WHERE Continent != ""
@@ -45,6 +50,7 @@ GROUP BY continent
 ORDER BY HighestDeathCount DESC;
 
 -- Global Numbers
+
 SELECT date,SUM(new_cases) AS TotalCases, SUM(new_deaths) AS TotalDeaths, SUM(new_deaths)/SUM(new_cases)*100 AS DeathPercentage
 FROM CovidDeaths.coviddeaths
 WHERE Continent != ""
@@ -52,6 +58,7 @@ GROUP BY date
 ORDER BY 1;
 
 -- Looking at Total Population VS Vaccination by joining two tables
+
 SELECT DEA.continent, DEA.location, DEA.date, DEA.population, VAC.new_vaccinations
 FROM CovidDeaths.coviddeaths AS DEA
 JOIN CovidDeaths.covidvaccinations AS VAC
@@ -88,7 +95,8 @@ SELECT *, (CUM_new_vaccinations/population)*100
 FROM PopvsVac;
 
 
--- Temp Table
+-- Creating new table
+
 CREATE TABLE PercentPopulationVaccinated
 (
 Continent nvarchar(255),
@@ -112,6 +120,7 @@ SELECT *, (CUM_new_vaccinations/population)*100
 FROM PercentPopulationVaccinated;
 
 -- Creating view to store data for visualizations
+
 CREATE VIEW PercentPopulationVaccinated AS
 SELECT DEA.continent, DEA.location, DEA.date, DEA.population, VAC.new_vaccinations
 , SUM(VAC.new_vaccinations) OVER (PARTITION BY DEA.location ORDER BY DEA.location, DEA.date) AS CUM_new_vaccinations
